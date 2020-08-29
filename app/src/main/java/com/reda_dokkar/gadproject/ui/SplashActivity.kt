0bc58217cbaa5c82c941dc32c0724cc16859dd95
DokkarRachidReda <hr_dokkar@esi.dz>
2020-08-29 02:28:08 +0100

    splash screen done

---
+++ app/src/main/java/com/reda_dokkar/gadproject/ui/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.reda_dokkar.gadproject.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
