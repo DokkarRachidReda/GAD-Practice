3ebc13c1f0da5eddf03e737fd7af116cc52a2dbd
DokkarRachidReda <hr_dokkar@esi.dz>
2020-08-31 02:48:38 +0100

    added submit function

--- app/src/main/java/com/reda_dokkar/gadproject/data/api/GadApi.kt
+++ app/src/main/java/com/reda_dokkar/gadproject/data/api/GadApi.kt
@@ -3,7 +3,7 @@ package com.reda_dokkar.gadproject.data.api
 import com.reda_dokkar.gadproject.data.model.LearningItem
 import com.reda_dokkar.gadproject.data.model.SkillItem
 import retrofit2.Call
-import retrofit2.http.GET
+import retrofit2.http.*
 
 interface GadApi {
 
@@ -14,4 +14,14 @@ interface GadApi {
 
     @GET("api/skilliq")
     fun getSkillIQLeaders(): Call<ArrayList<SkillItem>>
+
+
+    @POST
+    @FormUrlEncoded
+    fun submit(@Url url:String = "https://docs.google.com/forms/d/e/1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse",
+                @Field("entry.1824927963") email:String,
+                @Field("entry.1877115667") name:String,
+                @Field("entry.2006916086") lastName:String,
+                @Field("entry.284483984") link:String
+    ) :  Call<Any?>
 }
