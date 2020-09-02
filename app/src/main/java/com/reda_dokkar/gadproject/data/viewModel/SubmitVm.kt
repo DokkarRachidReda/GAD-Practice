5cc3988f66d5c5aa90b586c47daba464c3e40923
DokkarRachidReda <hr_dokkar@esi.dz>
2020-09-02 02:01:26 +0100

    completed alert dialog design

---
+++ app/src/main/java/com/reda_dokkar/gadproject/data/viewModel/SubmitVm.kt
@@ -0,0 +1,14 @@
+package com.reda_dokkar.gadproject.data.viewModel
+
+import androidx.lifecycle.ViewModel
+import com.reda_dokkar.gadproject.data.repository.BaseRepository
+
+class SubmitVm : ViewModel() {
+
+    val repo = BaseRepository()
+
+    fun submit(){
+
+
+    }
+}
