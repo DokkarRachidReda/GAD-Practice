996e988eda71108adba72dad7dd6fbdf9663e263
DokkarRachidReda <hr_dokkar@esi.dz>
2020-09-02 02:08:16 +0100

    completed submit activity view model

--- app/src/main/java/com/reda_dokkar/gadproject/data/viewModel/SubmitVm.kt
+++ app/src/main/java/com/reda_dokkar/gadproject/data/viewModel/SubmitVm.kt
@@ -1,5 +1,7 @@
 package com.reda_dokkar.gadproject.data.viewModel
 
+import android.util.Patterns
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.reda_dokkar.gadproject.data.repository.BaseRepository
 
@@ -7,8 +9,22 @@ class SubmitVm : ViewModel() {
 
     val repo = BaseRepository()
 
-    fun submit(){
+    fun submit(email:String,name:String,lastName:String,link:String){
 
+    if(Patterns.EMAIL_ADDRESS.matcher(email).matches() // validate email
+        && Patterns.WEB_URL.matcher(link).matches()   //  validate url
+        && link.contains("github")             //   check if it's a github url
+    ){
+
+        repo.submit(email, name, lastName, link)
+
+    }else{
+
+        repo.submitLiveData.value = false
+    }
 
     }
+
+
+    fun getSubmitLv():LiveData<Boolean>{return repo.submitLiveData}
 }
